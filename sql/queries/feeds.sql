3ca1663fed55fde18dd03d08ed88e398caf4add5
cryptrunner49 <cryptrunner49@proton.me>
2025-02-09 11:10:48 +0000

    Add Feed Scraping Feature
    - Introduced functionality to scrape rss feeds and parse them (currently RFC1123Z are supported).
    - Enhanced the feed parser interface to integrate seamlessly with the new scraping logic.

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -8,3 +8,15 @@ SELECT * FROM feeds;
 
 -- name: DeleteFeedFollow :exec
 DELETE FROM feed_follows WHERE id = $1 AND user_id = $2;
+
+-- name: GetNextFeedsToFetch :many
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT $1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
